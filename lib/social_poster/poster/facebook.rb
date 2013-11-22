0c11b3be7e88b4257ab22cd0d48cd87774f1abea
HeeL <parizhskiy@gmail.com>
2013-11-22 19:31:17 +0100

    post to groups on facebook

--- lib/social_poster/poster/facebook.rb
+++ lib/social_poster/poster/facebook.rb
@@ -11,9 +11,16 @@ module SocialPoster
       end
 
       def write(text, title)
-        FbGraph::User.me(config_key :access_token).feed!({message: text}.merge(@options))
+        if @options[:id]
+          id = @options.delete :id
+          FbGraph::User.new(id).feed!({message: text}.merge(@options).merge(access_token: (config_key :access_token)))
+        else
+          FbGraph::User.me(config_key :access_token).feed!({message: text}.merge(@options))
+        end
       end
     end
 
   end
 end
+
+
